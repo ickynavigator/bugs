794e3037e65b645fa5e2c11b127cd0cfe1d0e640
Obi Fortune <gabrielobi.of@gmail.com>
2024-03-03 19:58:28 -0500

    add turso setup

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,16 +1,21 @@
-import { Client } from '@planetscale/database';
-import { PrismaPlanetScale } from '@prisma/adapter-planetscale';
 import { PrismaClient } from '@prisma/client';
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
+import { createClient } from '@libsql/client';
 
 import { env } from '~/env';
 
-const psClient = new Client({ url: env.DATABASE_URL });
+const libsql = createClient({
+  url: env.DATABASE_URL,
+  authToken: env.DATABASE_TOKEN,
+});
+
+const adapter = new PrismaLibSQL(libsql);
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-    adapter: new PrismaPlanetScale(psClient),
+    adapter,
   });
 
 const globalForPrisma = globalThis as unknown as {
