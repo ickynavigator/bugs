9d4a19c0b090f2c8a1216850de683320c649fc9e
Obi Fortune <gabrielobi.of@gmail.com>
2024-03-07 18:00:33 -0500

    update google provider

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -87,6 +87,13 @@ export const {
     Google({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
     }),
   ],
 });
