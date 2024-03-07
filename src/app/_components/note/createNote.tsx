3234e0fd2c9fb0edb2cf140f6c6c574cbfea7a8d
Obi Fortune <gabrielobi.of@gmail.com>
2024-03-06 23:37:30 -0500

    add loader for notes page and a create note component

---
+++ src/app/_components/note/createNote.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Button } from '@mantine/core';
+import React from 'react';
+import { api } from '~/trpc/react';
+
+export default function CreateNote() {
+  const utils = api.useUtils();
+  const createNote = api.notes.createNote.useMutation({
+    onSuccess: () => {
+      void utils.notes.getNotes.invalidate();
+    },
+  });
+
+  return (
+    <Button
+      fullWidth
+      onClick={() => createNote.mutate()}
+      loading={createNote.isLoading}
+    >
+      Create new note
+    </Button>
+  );
+}
--- src/app/dashboard/notes/page.tsx
+++ src/app/dashboard/notes/page.tsx
@@ -1,18 +1,24 @@
-import { Stack } from '@mantine/core';
+'use client';
 
-import Note from '~/app/_components/note';
+import { Box, Loader, Stack } from '@mantine/core';
+import CreateNote from '~/app/_components/note/createNote';
+import Note from '~/app/_components/note/index';
+import { api } from '~/trpc/react';
 
-const content =
-  '<h2 style="text-align: center;">Welcome to Mantine rich text editor</h2><p><code>RichTextEditor</code> component focuses on usability and is designed to be as simple as possible to bring a familiar editing experience to regular users. <code>RichTextEditor</code> is based on <a href="https://tiptap.dev/" rel="noopener noreferrer" target="_blank">Tiptap.dev</a> and supports all of its features:</p><ul><li>General text formatting: <strong>bold</strong>, <em>italic</em>, <u>underline</u>, <s>strike-through</s> </li><li>Headings (h1-h6)</li><li>Sub and super scripts (<sup>&lt;sup /&gt;</sup> and <sub>&lt;sub /&gt;</sub> tags)</li><li>Ordered and bullet lists</li><li>Text align&nbsp;</li><li>And all <a href="https://tiptap.dev/extensions" target="_blank" rel="noopener noreferrer">other extensions</a></li></ul>';
+export default function Page() {
+  const notes = api.notes.getNotes.useQuery();
 
-const notes = [{ id: 1, content: content, name: 'Note 1' }];
-
-export default async function Page() {
   return (
     <Stack>
-      {notes.map(note => (
-        <Note key={note.id} content={note.content} name={note.name} />
-      ))}
+      <CreateNote />
+
+      {notes.isLoading ? (
+        <Box>
+          <Loader />
+        </Box>
+      ) : (
+        notes.data?.map(note => <Note key={note.id} note={note} />)
+      )}
     </Stack>
   );
 }
