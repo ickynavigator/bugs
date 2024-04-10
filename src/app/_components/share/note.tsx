11b9c380d05dd2fb852de97eaf14430f07b99d87
Obi Fortune <gabrielobi.of@gmail.com>
2024-04-10 01:39:24 -0400

    created a share icon

---
+++ src/app/_components/share/note.tsx
@@ -0,0 +1,36 @@
+import { ActionIcon, CopyButton, Tooltip, rem } from '@mantine/core';
+import { IconCheck, IconCopy } from '@tabler/icons-react';
+import { env } from '~/env';
+
+interface Props {
+  userId: string;
+  noteId: string | number;
+}
+
+export default function Note(props: Props) {
+  const { userId, noteId } = props;
+
+  const shareurl = new URL(
+    `${env.NEXT_PUBLIC_VERCEL_URL}/share/note/${userId}/${noteId}`,
+  );
+
+  return (
+    <CopyButton value={shareurl.toString()}>
+      {({ copied, copy }) => (
+        <Tooltip label={copied ? 'Copied' : 'Copy'} withArrow>
+          <ActionIcon
+            color={copied ? 'teal' : 'yellow'}
+            onClick={copy}
+            variant="outline"
+          >
+            {copied ? (
+              <IconCheck style={{ width: rem(16) }} />
+            ) : (
+              <IconCopy style={{ width: rem(16) }} />
+            )}
+          </ActionIcon>
+        </Tooltip>
+      )}
+    </CopyButton>
+  );
+}
